4da18f17ff268c21f33b7a7303bece11c429b79d
captn-hook <tristanskyhook@gmail.com>
2022-12-04 08:00:40 -0800

    WORDSERVER

--- Project10/chat_client.py
+++ Project10/chat_client.py
@@ -3,61 +3,139 @@ import threading
 import time
 import sys
 import socket
+import json
+import os
 
 from chatui import init_windows, read_command, print_message, end_windows
 #from chatuicurses import init_windows, read_command, print_message, end_windows
-
+#chatui bug? if the message is longer than the screen width, it will get pushed into the cleared line
+#if you do this more than once, the ghost lines will stack up
+nick = None
+s = None
 
 def usage():
-    print("usage: chat_client.py nickname host port", file=sys.stderr)
+    print("usage: chat_client.py nickname port host", file=sys.stderr)
     
 
-def runner():
-    count = 0
-
+def userin():
+    global nick
     while True:
-        time.sleep(2)
-        print_message(f"*** Runner count: {count}")
-        count += 1
+        try:
+            command = read_command(f"{nick}> ")
+        except:
+            break
+        
+        if command != '' and command[0] == '/':
+            #commands
+            #/q quit
+            if command == '/q':
+                end_windows()
+                os._exit(1)
+                sys.exit(0)
+                break
+        
+        print_message(f"{nick}: {command}")
+
+        #make payload
+        msg = {
+            "type": "chat",
+            "message": command
+        }
+
+        msg = packit(msg)
+        
+        #send the command to the server
+        try:
+            s.sendall(msg)
+        except:
+            print_message("Unable to send message")
+            break
+
+def packit(msg):
+    
+        msg = json.dumps(msg, indent=0)
+
+        n = len(msg)
+
+       
+        newmsg = bytearray(n.to_bytes(2, 'big')) + msg.encode()
+      
+        return newmsg
 
 def main(argv):
+    global nick
+    global s
     try:
         nick = argv[1]
-        host = argv[2]
-        port = int(argv[3])
+        port =int(argv[2])
+        if len(argv) > 3:
+            host = argv[3]
+        else:
+            host = "localhost"
     except:
         usage()
         return 1
             
     init_windows()
 
-    t1 = threading.Thread(target=runner, daemon=True)
-    t1.start()
-
-    # Make the client socket and connect
     s = socket.socket()
 
-    try:
-        s.connect((host, port))
-    except:
-        print_message("Unable to connect to server")
-        return 1
+
+    # Make the client socket and connect
+  
+
+    #try:
+    s.connect((host, port))
+
+    hellomsg = {
+        "type": "hello",
+        "nick": nick
+    }
+
+    hellomsg = packit(hellomsg)
+
+    s.sendall(hellomsg)
+        #s.sendall(hellomsg.encode())
+        
+    #except:
+    #    print_message("Unable to connect to server")
+    #    end_windows()
+    #    return 1
+
+    
+    t1 = threading.Thread(target=serverin, daemon=True)
+    t1.start()
+
+    userin()
+
+
+def serverin():
+
+    global s
+    global nick
 
     while True:
-        try:
-            command = read_command("Enter a thing> ")
-        except:
-            break
-        
-        if command[0] == '/':
-            #commands
-            #/q quit
-            if command == '/q':
-                break
-        else:
-            print_message(f"{nick}> {command}")
 
-    end_windows()
+        #recieve any response from the server
+        
+        try:
+            data = s.recv(1024)
+            data = json.loads(data.decode())
+                
+            if data["type"] == "chat" and data["nick"] != nick:
+                print_message(f"{data['nick']}: {data['message']}")
+
+            elif data["type"] == "join":
+                print_message(f"*** {data['nick']} has joined the chat")
+
+            elif data["type"] == "leave":
+                print_message(f"*** {data['nick']} has left the chat")
+        except:
+            print_message("Server has disconnected")
+            end_windows()
+            os._exit(1)
+            sys.exit(0)
+            break
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
--- Project10/chat_server.py
+++ Project10/chat_server.py
@@ -5,8 +5,17 @@
 import sys
 import socket
 import select
+import json
+
+server_socket = None
+
+packet_buffer = {}
 
 def run_server(host, port):
+
+    global server_socket
+    global packet_buffer
+
     #create socket
     server_socket = socket.socket()
     try:
@@ -19,7 +28,10 @@ def run_server(host, port):
     server_socket.listen()
 
     sockset = {server_socket}
+
     print("*** Server starting")
+
+    users = {}
     while True:
         #wait for input
 
@@ -32,35 +44,138 @@ def run_server(host, port):
 
                 sockset.add(new_sock)
 
-                print(f"{new_sock.getpeername()}: connected")
+                print(f"*** {new_sock.getpeername()}: connected")
 
             else:
-                #receive data
-                data = sock.recv(1024)
+                try:
+                    #receive data
+                    data = sock.recv(2)
+                    #if data bytes < 1024, then we have a full message
+                    if int.from_bytes(data, 'big') < 1024:
+                        data = sock.recv(1024)
+                        
+                        name = sock.getpeername()
 
-                name = sock.getpeername()
+                        name = f"{name[0]}:{name[1]}"
 
-                if data:
+                        if data:
 
-                    data = data.decode()
+                            data =  json.loads(data.decode())
 
-                    print(f"{name}: {len(data)} bytes: {data}")
+                            if data["type"] == "hello":
+                                print(f"*** {name}: {len(data)} bytes: {data['type']}: {data['nick']}")
+                                
+                                try:
+                                    try:
+                                        key = next(key for key, value in users.items() if value == data['nick'])
+                                        print(f"*** {name}: {data['nick']} already in use by {key}")
+                                        remove(sock, sockset)
 
-                else:
-                    
-                    print(f"{sock.getpeername()}: disconnected")
+                                    except StopIteration:
+                                        n = users[name]
 
-                    sock.close()
+                                except KeyError:
+                                    users[name] = data['nick']
+                                    packet_buffer[name] = b''
+                                    print(f"{name}: {users[name]}> connected")
 
-                    sockset.remove(sock)
+                                    msg = {
+                                        "type": "join",
+                                        "nick": data['nick']
+                                    }
 
+                                    for s in sockset:
+                                        if s != server_socket and s != sock:
+                                            s.sendall(json.dumps(msg, indent=0).encode())
+
+                            
+
+                            elif users[name]:
+
+                                nick = users[name]                   
+
+                                print(f"{name}: {nick}> {data['message']}")
+
+                                msg = {
+                                    "type": "chat",
+                                    "nick": nick,
+                                    "message": data["message"]
+                                }
+
+                                msg = json.dumps(msg, indent=0).encode()
+
+                                for s in sockset:
+                                    if s != server_socket and s != sock:
+                                        s.sendall(msg)
+
+                        else:
+                            remove(sock, sockset, users)
+
+                except:
+                    remove(sock, sockset, users)
+
+
+def get_next_word_packet(s, name):
+
+    global packet_buffer
+
+    length = int.from_bytes(packet_buffer[:2], 'big')
+
+    while len(packet_buffer) < length + WORD_LEN_SIZE:
+
+        packet_buffer += s.recv(5)
+
+        length = int.from_bytes(packet_buffer[:WORD_LEN_SIZE], 'big')
+
+        if len(packet_buffer) == 0:
+
+            return None
+
+    word = packet_buffer[:length + WORD_LEN_SIZE]
+
+    packet_buffer = packet_buffer[length + WORD_LEN_SIZE:]
+
+    return word
+
+def remove(sock, sockset, users=None):
+    name = sock.getpeername()
+    name = f"{name[0]}:{name[1]}"
+
+    global server_socket
+
+    if users:
+        try:
+            nick = users[name]
+            del users[name]
+
+            msg = {
+                "type": "leave",
+                "nick": nick
+            }
+
+            msg = json.dumps(msg, indent=0).encode()
+
+            for s in sockset:
+                if s != server_socket and s != sock:
+                    s.sendall(msg)
+        except KeyError:
+            pass    
+
+    
+    print(f"*** {name}: disconnected")
+    sock.close()
+    sockset.remove(sock)
+    
 def usage():
-    print("usage: chat_server.py host port", file=sys.stderr)
+    print("usage: chat_server.py port host", file=sys.stderr)
     
 def main(argv):
     try:
-        host = argv[1]
-        port = int(argv[2])
+        port = int(argv[1])
+        if len(argv) > 2:
+            host = argv[2]
+        else:
+            host = "localhost"
     except:
         usage()
         return 1
